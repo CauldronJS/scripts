84cee5950b7d726a65cffef0092e4f456751a350
Justin Cox <dubstepandponies95@gmail.com>
2019-04-16 22:48:57 -0400

    Honestly I'm a fucking idiot lmao

---
+++ docs/_coverpage.md
@@ -0,0 +1,4 @@
+# Cauldron
+
+<small>v1.0</small>
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import cauldron from 'cauldron';
+import { MongoClient } from 'mongodb';
 
 cauldron.initialize();
 cauldron.events.server.on('listping', ({ setMotd }) => setMotd('Testing'));
+
+const client = new MongoClient('mongodb://localhost:27017');
+client.connect(err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('Successfully connected to mongo');
+  const db = client.db('cauldron');
+});
