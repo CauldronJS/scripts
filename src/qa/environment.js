fcf455b458b19e1a53800711351e7609e5b2a620
Justin Cox <dubstepandponies95@gmail.com>
2019-04-23 23:22:25 -0400

    start work on QA plugin

---
+++ src/qa/environment.js
@@ -0,0 +1,9 @@
+
+/**
+ *
+ * @param {*} tester The player that is the tester
+ * @param {(err, world) => void} callback The callback to be ran when the generation completes
+ */
+const createQAWorld = (tester, callback) => {
+
+}
---
+++ src/qa/index.js
@@ -0,0 +1,4 @@
+// This package is for QAing content in our MC server
+import dotenv from 'dotenv';
+
+dotenv.config();
