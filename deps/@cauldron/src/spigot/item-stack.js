3642b60063af650927b057d0e064101a5ad19653
Justin Cox <jcox2@wayfair.com>
2019-06-18 14:25:21 -0400

    I don't even know anymore

---
+++ deps/@cauldron/src/spigot/item-stack.js
@@ -0,0 +1,56 @@
+import BukkitItemStack from '@java/org.bukkit.inventory.ItemStack';
+
+function getItemStack(data) {
+  // this will accept either a string of the material name,
+  // an object with { id, meta }, { id, damage }, or a
+  // CauldronItem object
+  return data;
+}
+
+function getEnchantments(data) {}
+
+export default class ItemStack {
+  constructor(
+    type,
+    {
+      amount = 1,
+      enchantments = []
+    }
+  ) {
+    this.$$baseItemStack = getItemStack(type);
+    this.$$baseItemStack.setAmount(amount);
+  }
+
+  get amount() {
+    return this.$$baseItemStack.getAmount();
+  }
+
+  set amount(value) {
+    this.$$baseItemStack.setAmount(value);
+  }
+
+  get enchantments() {
+    return this.$$baseItemStack.getEnchantments();
+  }
+
+  set enchantments(value) {
+    this.$$baseItemStack.setEnchantments(value);
+  }
+
+  get type() {
+    return this.$$baseItemStack.getType();
+  }
+
+  set type(value) {
+    this.$$baseItemStack.setType(value);
+  }
+
+  get maxStackSize() {
+    return this.$$baseItemStack.getMaxStackSize();
+  }
+
+  isSimilar(itemStack) {
+    return this.$$baseItemStack.isSimilar(getItemStack(itemStack));
+  }
+}
+
---
+++ deps/@cauldron/src/spigot/recipes.js
@@ -0,0 +1,32 @@
+import {
+  BlastingRecipe,
+  CampfireRecipe,
+  FurnaceRecipe,
+  MerchantRecipe,
+  ShapedRecipe,
+  ShapelessRecipe,
+  SmokingRecipe,
+  StonecuttingRecipe,
+  RecipeChoice
+} from '@java/org.bukkit.inventory';
+import { Material } from '@java/org.bukkit';
+import { NAMESPACE_KEY } from '@cauldron';
+import ItemStack from './item-stack';
+
+export const blastingRecipe = ({
+  result,
+  input,
+  experience = 0,
+  cookingTime = 20
+}) => new BlastingRecipe(NAMESPACE_KEY);
+
+export const campfireRecipe = ({
+  result,
+  input,
+  experience = 0,
+  cookingTime = 20
+}) => new CampfireRecipe(NAMESPACE_KEY);
+
+export const furnaceRecipe = ({
+  result
+}) => new FurnaceRecipe(NAMESPACE_KEY);
