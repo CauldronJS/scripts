3b87130004c4b23165118480ade68ef38a3cb51b
Conji <dubstepandponies95@gmail.com>
2019-09-02 20:50:11 -0400

    More HTTP work

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import JsCommand from './js';
 import colors from '@cauldron/colors';
 import '@cauldron/captcha';
 import Axios from 'axios';
+import express from 'express';
 
 cauldron();
 
@@ -22,6 +23,14 @@ const App = () => (
 );
 
 Rinse.mount(<App />);
-Axios.get('https://google.com')
-  .then(console.log)
-  .catch(err => console.error(err.toString()));
+
+const app = express();
+app.use(express.static('./site/public'));
+app.use('/css', express.static('./site/public/css'));
+app.use('/js', express.static('./site/public/js'));
+app.use('/images', express.static('./site/public/images'));
+
+const server = app.listen(25566, () => {
+  const port = server.address().port;
+  console.log(`Web server started on http://localhost:${port}`);
+});
