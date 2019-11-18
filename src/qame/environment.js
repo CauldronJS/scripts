cbbea4d8f647be41d0079f8a3ed9b4942b65c176
Conji <dubstepandponies95@gmail.com>
2019-11-18 17:41:45 -0500

    Yeet, now I can hide these files (but that means no updates for them either)

--- src/qame/environment.js
+++
@@ -1,5 +0,0 @@
-import { events, useStore } from 'cauldron';
-
-function environment() {
-  const [store, setStore] = useStore('qame');
-}
--- src/qame/index.js
+++
@@ -1,5 +0,0 @@
-// This package is for QAing content in our MC server
-import QaCommand from './commands';
-import Rinse from '@cauldron/rinse';
-
-Rinse.mount(<QaCommand />);
