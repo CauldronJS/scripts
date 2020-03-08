af00fca5a4e9971623af83c94a187c03bc514110
Conji <dubstepandponies95@gmail.com>
2020-03-07 23:13:08 -0500

    Boutta start working on HTTP. Again.

--- src/spigot/index.js
+++ src/spigot/index.js
@@ -3,6 +3,7 @@ import RtpCommand from './rtp';
 import JsCommand from './js';
 import colors from '@cauldron/colors';
 import cauldron from 'cauldron';
+import axios from 'axios';
 // import express from 'express';
 
 function setMotd({ setMotd }) {
@@ -21,7 +22,21 @@ const App = () => (
 Rinse.mount(<App />);
 
 (async function() {
-  await $$isolate$$.generateAsyncPromise(() => console.log(42));
+  try {
+    $$isolate$$
+      .getAsyncFactory()
+      .wait(5000)
+      .then(() => console.log('This took 5 seconds to run'));
+    $$isolate$$
+      .getAsyncFactory()
+      .generateAsyncPromise(() =>
+        console.log('This ran as quickly as possible')
+      );
+    const result = await axios.get('https://google.com');
+    console.log(result.data);
+  } catch (err) {
+    console.error(err);
+  }
 })();
 
 // const app = express();
