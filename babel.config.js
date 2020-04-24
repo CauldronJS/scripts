a282bfa2c710e66d161c38229794798e06e6d1b9
Conji <dubstepandponies95@gmail.com>
2020-04-24 10:42:30 -0400

    Add optional chaining to project

--- babel.config.js
+++ babel.config.js
@@ -9,7 +9,8 @@ module.exports = api => {
         { pragma: 'Rinse.createComponent', pragmaFrag: 'Rinse.Fragment' }
       ],
       '@babel/plugin-transform-modules-commonjs',
-      '@babel/plugin-proposal-class-properties'
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-optional-chaining'
     ]
   };
   return config;
